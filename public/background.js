52c305ef914ba0bbc8af0c2b281f1014a5500097
Harsh Gupta <hg242322@gmail.com>
2021-11-18 10:22:08 +0530

    initial commit

---
+++ public/background.js
@@ -0,0 +1,20 @@
+chrome.browserAction.onClicked.addListener(function (tab) {
+  chrome.tabs.sendMessage(tab.id, { message: "load" });
+});
+
+chrome.browserAction.setIcon({ path: "./favicon.ico" });
+
+// function callLocalhost(info, tab) {
+//   chrome.tabs.update(info.tab, {
+//     url: `http://localhost:3000/?url=${info.pageUrl}`,
+//   });
+//   chrome.tabs.sendMessage(tabs[0].id, {
+//     url: `http://localhost:3000/?url=${info.pageUrl}`,
+//   });
+// }
+
+// var child1 = chrome.contextMenus.create({
+//   title: "Chat here",
+//   onclick: callLocalhost,
+//   contexts: ["all"],
+// });
